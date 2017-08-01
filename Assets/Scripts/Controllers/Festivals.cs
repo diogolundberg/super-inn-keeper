93a02a8f623e098c37021dcc9aeb3d0ba838a810
Diogo Lundberg <diogo@lundberg.com.br>
2017-08-01 01:27:48 -0300

    Initial commit

---
+++ Assets/Scripts/Controllers/Festivals.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controllers
+{
+    public class Festivals : MonoBehaviour
+    {
+        public RectTransform festivals;
+        public GameObject festivalRow;
+
+        // Use this for initialization
+        void Start()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject row = (GameObject)Instantiate(festivalRow);
+                row.transform.SetParent(festivals, false);
+                Button button = row.GetComponent<Button>();
+
+                button.onClick.AddListener(() => Debug.Log("Button clicked"));
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class Menu : MonoBehaviour
+    {
+
+        public GameObject screen;
+
+        void Start()
+        {
+            Activate("Festivals");
+        }
+
+        public void Activate(string name)
+        {
+            foreach (Transform transform in screen.transform)
+            {
+                GameObject gameObject = transform.gameObject;
+                gameObject.SetActive(gameObject.name == name);
+            }
+        }
+    }
+}
